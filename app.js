ac717a85e69762ec794892a3f122fdb402088afe
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-07-08 22:15:14 +0200

    setup project.

---
+++ app.js
@@ -0,0 +1,14 @@
+// impports
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the event web page.");
+});
+
+app.listen(3000);
