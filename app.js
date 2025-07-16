c1621e4c6371fc52455e348d0c1c78bd889148fa
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-07-16 16:48:28 +0200

    worked on template engine.

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const morgan = require("morgan");
 const path = require("path");
 // Import event routes
 const eventRoutes = require("./routes/events.js");
-const attendeeRoutes = require("./routes/attendees"); 
-
-app.use("/events", eventRoutes);
-app.use("/attendees", attendeeRoutes); 
+const attendeeRoutes = require("./routes/attendees");
 
 // Middleware to log requests
 app.use(morgan("short"));
 app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the public directory
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 
+app.use("/events", eventRoutes);
+app.use("/attendees", attendeeRoutes);
+
 // template engine setup
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Accueil", h1: "Event Belgium" }); // Render the index.pug template
+  res.render("index"); // Render the index.pug template
 });
 
 app.listen(3000);
